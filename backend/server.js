f1a7b930f66bc1c7957eef9726884cc947174f5c
Jayanne Nery <jayanne.conceicao@ba.estudante.senai.br>
2026-09-24 21:18:04 +0000

    desafio

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
-  res.json({
+  res.json([{
     nome: "Celular",
     preco: 1800,
     categoria: "Eletrônicos"
-  });
+  }]);
 });
 
 app.listen(3000, () => {
